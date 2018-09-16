7c4a6a562518c3ceef84355b1a7d1c081810091f
William Zhu <mingyangzhu2011@gmail.com>
2018-09-16 14:54:27 -0400

    moved state machine files to own folder

---
+++ lab2.py
@@ -0,0 +1,42 @@
+
+
+import sys
+import cozmo
+import datetime
+import time
+
+import asyncio
+def run(sdk_conn):
+    
+    robot = sdk_conn.wait_for_robot()
+    robot.camera.image_stream_enabled = True
+    robot.camera.color_image_enabled = False
+    robot.camera.enable_auto_exposure()
+
+    robot.set_head_angle(cozmo.util.degrees(0)).wait_for_completed()
+
+    
+    """
+    drone
+    """
+
+
+
+    """
+    order
+    """
+
+
+
+    """
+    inspection
+    """
+    
+
+if __name__ == '__main__':
+    cozmo.setup_basic_logging()
+
+    try:
+        cozmo.connect(run)
+    except cozmo.ConnectionError as e:
+        sys.exit("A connection error occurred: %s" % e)
