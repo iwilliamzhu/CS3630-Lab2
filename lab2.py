b7d2019f8fc8a60b7b7b929134b5d06af5db23be
Andy Leekung <chingyeuleekung@gmail.com>
2018-09-16 15:25:54 -0400

    Move classifying image to a new file

--- lab2.py
+++ lab2.py
@@ -4,6 +4,9 @@ Mingyang Zhu
 '''
 
 import sys
+
+sys.path.append('./State')
+
 import cozmo
 import datetime
 import time
@@ -11,7 +14,8 @@ import numpy as np
 from sklearn.externals import joblib
 from skimage import io, feature, filters, exposure, color
 
-import simple_device
+from state_machine import StateMachine
+import classifyimage
 
 def run(sdk_conn):
     clf = joblib.load('classifier.pkl')
@@ -24,34 +28,16 @@ def run(sdk_conn):
     robot.set_head_angle(cozmo.util.degrees(0)).wait_for_completed()
 
     # instantiate simple device in Idle State
-    device = SimpleDevice()
+    device = StateMachine()
 
     while True:
         time.sleep(4)
         raw_image = robot.world.latest_image.raw_image
-        features = extract_image_features([np.array(raw_image)])
-        predicted_label = clf.predict(features)[0]
+        predicted_label = classifyimage.classify_image(raw_image)
         print(predicted_label)
         if predicted_label != 'none':
             robot.say_text(predicted_label).wait_for_completed()
             device.on_event(predicted_label)
-
-
-def extract_image_features(data):
-    l = []
-    for im in data:
-        im_gray = color.rgb2gray(im)
-    
-        im_gray = filters.gaussian(im_gray, sigma=0.4)
-        
-        f = feature.hog(im_gray, orientations=10, pixels_per_cell=(48, 48), cells_per_block=(4, 4), feature_vector=True, block_norm='L2-Hys')
-        l.append(f)
-    
-
-    feature_data = np.array(l)
-    return(feature_data)
-
-def 
 
 if __name__ == '__main__':
     cozmo.setup_basic_logging()
